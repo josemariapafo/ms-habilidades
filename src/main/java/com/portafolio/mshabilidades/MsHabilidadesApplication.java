1c8b86422a77aa11b2b5ef32cc9b3e114fc8db3b
josemariapafo <parrillajosemaria@gmail.com>
2023-06-09 14:12:54 +0200

    Creacion micro ms-habilidades

---
+++ src/main/java/com/portafolio/mshabilidades/MsHabilidadesApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio.mshabilidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHabilidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHabilidadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portafolio/mshabilidades/MsHabilidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.mshabilidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHabilidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
